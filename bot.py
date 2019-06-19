efe0f684ce44c446c3f5bc835d4a233b4ca8964a
Calvin C <lefrogjones@gmail.com>
2019-06-19 17:28:16 -0400

    Notify users at specified time before next cycle

--- bot.py
+++ bot.py
@@ -9,7 +9,10 @@ import asyncio
 
 bot = commands.Bot(command_prefix='!')
 token = config.DISCORD_TOKEN
-cetus_list = []
+
+# Users to notify for next day/night cycle // user: [time before to notify, has been notified]
+cetus_dict = {}
+is_day = True
 
 
 # Single session
@@ -31,7 +34,13 @@ async def on_ready():
 
     # Periodically check
     while True:
-        await asyncio.gather(check_cycle(2))
+        await asyncio.gather(check_cycle(20))
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Invalid command.")
 
 
 # Check the status of the current cycle and remaining time left
@@ -46,25 +55,30 @@ async def cycle(ctx):
     await ctx.send(f'Currently: {current_cycle}\nTime Left: {time_left}')
 
 
-# Add user of command to the cetus_list to be notified of next day/night cycle
+# Add user of command to the cetus_dict to be notified of next day/night cycle, default time to notify will be 5 minutes
 @bot.command()
-async def alert_cycle(ctx):
-    if ctx.message.author not in cetus_list:
-        cetus_list.append(ctx.message.author)
-        await ctx.send(ctx.message.author.mention + ' You will now be alerted before the next Cetus day/night cycle.')
-    else:
-        await ctx.send(ctx.message.author.mention + ' You are already being alerted for the next Cetus day/night cycle.')
+async def alrtcycle(ctx, time='5'):
+    try:
+        if int(time) > 59 or int(time) < 1:
+            await ctx.send(ctx.message.author.mention + ' Enter a time between 1-59')
+        else:
+            cetus_dict[ctx.message.author] = [time, False]
+            await ctx.send(
+                ctx.message.author.mention +
+                f' You will now be alerted {time} minutes before the next Cetus day/night cycle.')
+    except ValueError:
+        await ctx.send(ctx.message.author.mention + ' Enter a time between 1-59.')
 
 
-# Remove user of command from the cetus_list to no longer be notified of next day/night cycle
+# Remove user of command from the cetus_dict to no longer be notified of next day/night cycle
 @bot.command()
-async def remove_cycle(ctx):
-    if ctx.message.author not in cetus_list:
-        await ctx.send(ctx.message.author.mention + ' You aren\'t even being alerted!')
-    else:
-        cetus_list.remove(ctx.message.author)
+async def rmcycle(ctx):
+    try:
+        cetus_dict.pop(ctx.message.author)
         await ctx.send(ctx.message.author.mention + ' You are no longer being alerted'
                                                     ' for the next Cetus day/night cycle.')
+    except KeyError:
+        await ctx.send(ctx.message.author.mention + ' You are currently not being alerted.')
 
 
 # Check time until next cycle, notify if needed // This will be called periodically on_ready
@@ -72,13 +86,35 @@ async def check_cycle(delay):
     await asyncio.sleep(delay)
     # message/ping user below
     cetus_cycle = await endpoint('cetusCycle')
+
+    # Check for time left (if has an hour or no minutes, default to 100 time_check
     time_left = cetus_cycle.get('timeLeft')
+    if time_left.__contains__('h') or not time_left.__contains__('m'):
+        time_check = 100  # Will never notify for 100 as notify range is 1-59
+    else:
+        time_check = time_left.split('m')[0]  # Only check for minutes
+
+    # Check for next cycle
     if cetus_cycle.get('isDay'):
         next_cycle = 'Night'
     else:
         next_cycle = 'Day'
-    for user in cetus_list:
-        await user.send(f'{time_left} until {next_cycle}.')
+
+    # Notify each user if time left matches their time to notify
+    # Do not notify user if they have already been notified in this night/day cycle
+    for user in cetus_dict.keys():
+        user_list = cetus_dict.get(user)
+        if user_list[0] == time_check and not user_list[1]:
+            cetus_dict[user][1] = True
+            await user.send(f'{time_left} until {next_cycle}.')
+
+    # Check if current cycle matches last cycle check
+    # If not, update cycle and reset has notified for all users to False
+    global is_day
+    if cetus_cycle.get('isDay') != is_day:
+        is_day = cetus_cycle.get('isDay')
+        for user in cetus_dict:
+            cetus_dict[user][1] = False
 
 
 bot.run(token)
